b069f1ecea435966b8a5946a7e6abf3d76a599e0
Chepubelja <rehusevych@ucu.edu.ua>
2020-02-12 21:09:13 +0200

    Added representation of the tweet as Bag-of-Words

--- solar-panel-emulator/src/main/scala/ua/ucu/edu/twitter_data_preparation.scala
+++ solar-panel-emulator/src/main/scala/ua/ucu/edu/twitter_data_preparation.scala
@@ -16,20 +16,29 @@ object twitter_data_preparation extends App {
                              time: String,
                              tweet: String)
 
+    case class TwitterRecordProcessed(date: String,
+                             time: String,
+                             tweet: String,
+                             bag_of_words: Map[String, Int])
+
     implicit val tweetFormat: RootJsonFormat[TwitterRecord] = jsonFormat3(TwitterRecord)
 
     val musk_twitter_records: List[TwitterRecord] = tweets_array.convertTo[List[TwitterRecord]]
-    var musk_twitter_processed_records: List[TwitterRecord] = List()
+    var musk_twitter_processed_records: List[TwitterRecordProcessed] = List()
 
     for (twitter_record <- musk_twitter_records) {
         val date_transformed = twitter_record.date.replace('.', '-')
 
         val tweet_filtered = twitter_record.tweet
-          .replaceAll("""[.!?\\/;,—\-_():]""", "")
-          .replaceAll("""(?m)\s+$""", "")
-          .toLowerCase
-        musk_twitter_processed_records = musk_twitter_processed_records :+ TwitterRecord(date_transformed, twitter_record.time, tweet_filtered)
+            .replaceAll("""[.!?\\/;,—\-_():]""", "")
+            .replaceAll("""(?m)\s+$""", "")
+            .toLowerCase
+
+        val words = tweet_filtered.split(" ")
+        val bag_of_words = words.groupBy((word: String) => word).mapValues(_.length)
+
+        musk_twitter_processed_records = musk_twitter_processed_records :+ TwitterRecordProcessed(date_transformed, twitter_record.time, tweet_filtered, bag_of_words)
     }
 
-    musk_twitter_processed_records.foreach(record => println("Date: %s - Time: %s - Tweet: %s".format(record.date, record.time, record.tweet)))
+    musk_twitter_processed_records.foreach(record => println("Date: %s - Time: %s - Bag-of-Words: %s".format(record.date, record.time, record.bag_of_words)))
 }
