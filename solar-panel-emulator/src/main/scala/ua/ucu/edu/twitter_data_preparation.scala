ea221a9526d16007153da698238b4bf252ae025d
Chepubelja <rehusevych@ucu.edu.ua>
2020-02-12 21:43:19 +0200

    Changed type of date to java.util.Date, added new case class for result records

--- solar-panel-emulator/src/main/scala/ua/ucu/edu/twitter_data_preparation.scala
+++ solar-panel-emulator/src/main/scala/ua/ucu/edu/twitter_data_preparation.scala
@@ -1,5 +1,13 @@
 package ua.ucu.edu
 
+// For date formatting
+import java.text.SimpleDateFormat
+import java.util.Date
+
+// For break and continue
+import util.control.Breaks._
+
+// For json parsing
 import spray.json._
 import DefaultJsonProtocol._
 
@@ -12,33 +20,53 @@ object twitter_data_preparation extends App {
     val tweets_json_str = try tweets_json.mkString finally tweets_json.close()
     var tweets_array = tweets_json_str.stripMargin.parseJson
 
+    private val dateFormat = "dd-MM-yyyy"
+
     case class TwitterRecord(date: String,
                              time: String,
                              tweet: String)
 
-    case class TwitterRecordProcessed(date: String,
-                             time: String,
-                             tweet: String,
-                             bag_of_words: Map[String, Int])
+    case class TwitterRecordProcessed(date: Date,
+                                      time: String,
+                                      tweet: String,
+                                      bag_of_words: Map[String, Int])
 
     implicit val tweetFormat: RootJsonFormat[TwitterRecord] = jsonFormat3(TwitterRecord)
 
     val musk_twitter_records: List[TwitterRecord] = tweets_array.convertTo[List[TwitterRecord]]
-    var musk_twitter_processed_records: List[TwitterRecordProcessed] = List()
+    var musk_twitter_records_processed: List[TwitterRecordProcessed] = List()
 
     for (twitter_record <- musk_twitter_records) {
         val date_transformed = twitter_record.date.replace('.', '-')
 
-        val tweet_filtered = twitter_record.tweet
-            .replaceAll("""[.!?\\/;,—\-_():]""", "")
-            .replaceAll("""(?m)\s+$""", "")
-            .toLowerCase
+        breakable {
+            if (date_transformed == "[]") {
+                break
+            }
+            else {
+                val date_changed_type = new SimpleDateFormat(dateFormat).parse(date_transformed)
+                val tweet_filtered = twitter_record.tweet
+                  .replaceAll("""[.!?\\/;,—\-_():]""", "")
 
-        val words = tweet_filtered.split(" ")
-        val bag_of_words = words.groupBy((word: String) => word).mapValues(_.length)
+                  // TODO Improve stripping extra whitespaces
+                  .replaceAll("""(?m)\s+$""", "")
+                  .toLowerCase
 
-        musk_twitter_processed_records = musk_twitter_processed_records :+ TwitterRecordProcessed(date_transformed, twitter_record.time, tweet_filtered, bag_of_words)
+                val words = tweet_filtered.split(" ")
+                val bag_of_words = words.groupBy((word: String) => word).mapValues(_.length)
+
+                musk_twitter_records_processed = musk_twitter_records_processed :+ TwitterRecordProcessed(date_changed_type, twitter_record.time, tweet_filtered, bag_of_words)
+            }
+        }
     }
 
-    musk_twitter_processed_records.foreach(record => println("Date: %s - Time: %s - Bag-of-Words: %s".format(record.date, record.time, record.bag_of_words)))
+    musk_twitter_records_processed.foreach{
+        record =>
+            println("Date: %s - Time: %s - Bag-of-Words: %s"
+              .format(
+                  new SimpleDateFormat(dateFormat).format(record.date),
+                  record.time,
+                  record.bag_of_words)
+            )
+    }
 }
--- weather-provider/src/main/scala/ua/ucu/edu/stocks_data_preparation.scala
+++ weather-provider/src/main/scala/ua/ucu/edu/stocks_data_preparation.scala
@@ -1,5 +1,10 @@
 package ua.ucu.edu
 
+// For date formatting
+import java.text.SimpleDateFormat
+import java.util.Date
+
+// For json parsing
 import spray.json._
 import DefaultJsonProtocol._
 
@@ -12,6 +17,8 @@ object stocks_data_preparation extends App {
   val stocks_json_str = try stocks_json.mkString finally stocks_json.close()
   var stocks_array = stocks_json_str.stripMargin.parseJson
 
+  private val dateFormat = "yyyy-MM-dd"
+
   case class StocksRecord(date: String,
                            open: Float,
                            high: Float,
@@ -22,9 +29,37 @@ object stocks_data_preparation extends App {
                            dividend_amount: Float,
                            split_coefficient: Float)
 
+  case class StocksRecordProcessed(date: Date,
+                                   open: Float,
+                                   high: Float,
+                                   low: Float,
+                                   close: Float,
+                                   adjusted_close: Float,
+                                   volume: Float,
+                                   dividend_amount: Float,
+                                   split_coefficient: Float)
+
   implicit val StocksRecordFormat: RootJsonFormat[StocksRecord] = jsonFormat9(StocksRecord)
 
   val tesla_stocks: List[StocksRecord] = stocks_array.convertTo[List[StocksRecord]]
+  var tesla_stocks_processed: List[StocksRecordProcessed] = List()
 
-  tesla_stocks.foreach(record => println("Date: %s -  Open: %s - Close: %s".format(record.date, record.open, record.close)))
+  for (tesla_stock_record <- tesla_stocks) {
+    val date_changed_type = new SimpleDateFormat(dateFormat).parse(tesla_stock_record.date)
+    tesla_stocks_processed = tesla_stocks_processed :+
+      StocksRecordProcessed(
+        date_changed_type, tesla_stock_record.open, tesla_stock_record.high,
+        tesla_stock_record.low, tesla_stock_record.close, tesla_stock_record.adjusted_close,
+        tesla_stock_record.volume, tesla_stock_record.dividend_amount, tesla_stock_record.split_coefficient)
+  }
+
+  tesla_stocks_processed.foreach{
+    record =>
+      println("Date: %s - Open: %s - Close: %s"
+        .format(
+          new SimpleDateFormat(dateFormat).format(record.date),
+          record.open,
+          record.close)
+      )
+  }
 }
