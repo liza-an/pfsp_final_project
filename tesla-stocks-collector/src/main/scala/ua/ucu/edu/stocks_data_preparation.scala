65780e58782f73c4b0675d30309797a4a0034a14
Chepubelja <rehusevych@ucu.edu.ua>
2020-02-13 21:13:03 +0200

    Added Tesla stocks getByDate()

--- tesla-stocks-collector/src/main/scala/ua/ucu/edu/stocks_data_preparation.scala
+++ tesla-stocks-collector/src/main/scala/ua/ucu/edu/stocks_data_preparation.scala
@@ -11,9 +11,9 @@ import DefaultJsonProtocol._
 object stocks_data_preparation extends App {
 
   // Change to your path
-  val path_to_stocks_json = "C:\\Users\\orest.rehusevych\\Documents\\Masters\\Scala\\pfsp_final_project\\data\\tesla_stocks_final.json"
+  val path_to_stocks_json = "tesla_stocks_final.json"
 
-  val stocks_json = scala.io.Source.fromFile(path_to_stocks_json)
+  val stocks_json = scala.io.Source.fromResource(path_to_stocks_json)
   val stocks_json_str = try stocks_json.mkString finally stocks_json.close()
   var stocks_array = stocks_json_str.stripMargin.parseJson
 
@@ -29,37 +29,26 @@ object stocks_data_preparation extends App {
                            dividend_amount: Float,
                            split_coefficient: Float)
 
-  case class StocksRecordProcessed(date: Date,
-                                   open: Float,
-                                   high: Float,
-                                   low: Float,
-                                   close: Float,
-                                   adjusted_close: Float,
-                                   volume: Float,
-                                   dividend_amount: Float,
-                                   split_coefficient: Float)
-
   implicit val StocksRecordFormat: RootJsonFormat[StocksRecord] = jsonFormat9(StocksRecord)
 
   val tesla_stocks: List[StocksRecord] = stocks_array.convertTo[List[StocksRecord]]
-  var tesla_stocks_processed: List[StocksRecordProcessed] = List()
+  var tesla_stocks_processed: Map[Date, Float] = Map()
 
   for (tesla_stock_record <- tesla_stocks) {
     val date_changed_type = new SimpleDateFormat(dateFormat).parse(tesla_stock_record.date)
-    tesla_stocks_processed = tesla_stocks_processed :+
-      StocksRecordProcessed(
-        date_changed_type, tesla_stock_record.open, tesla_stock_record.high,
-        tesla_stock_record.low, tesla_stock_record.close, tesla_stock_record.adjusted_close,
-        tesla_stock_record.volume, tesla_stock_record.dividend_amount, tesla_stock_record.split_coefficient)
+    tesla_stocks_processed += date_changed_type -> tesla_stock_record.open
   }
 
-  tesla_stocks_processed.foreach{
-    record =>
-      println("Date: %s - Open: %s - Close: %s"
+  def getStocksByDate(date: Date): Float = tesla_stocks_processed(date)
+
+  // Unprint if u want to test
+
+  tesla_stocks_processed.keys.foreach{
+    date =>
+      println("Date: %s - Open: %s"
         .format(
-          new SimpleDateFormat(dateFormat).format(record.date),
-          record.open,
-          record.close)
+          new SimpleDateFormat(dateFormat).format(date),
+          getStocksByDate(date))
       )
   }
 }
